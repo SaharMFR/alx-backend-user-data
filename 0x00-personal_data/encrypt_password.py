36f089dc4372c453163351c7eadcb701950f137d
SaharMFR <sweetysahora@gmail.com>
2024-07-05 00:27:20 +0000

    Task-5 0x00

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Passwords """
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """ Returns a salted, hashed password, which is a byte string """
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
