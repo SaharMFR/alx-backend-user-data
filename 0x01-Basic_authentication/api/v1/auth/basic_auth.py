174b0f00c5f23cd4865b2a01c501aab0735e8b06
SaharMFR <sweetysahora@gmail.com>
2024-07-09 22:52:52 +0000

    Task-8 0x01

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -16,3 +16,17 @@ class BasicAuth(Auth):
                 authorization_header[:6] != "Basic "):
             return None
         return authorization_header[6:]
+
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """ Decodes the Base64 part of the `Authorization` header """
+        if (base64_authorization_header is None or
+                not isinstance(base64_authorization_header, str)):
+            return None
+        try:
+            return (base64.b64decode(base64_authorization_header)
+                    .decode('utf-8'))
+        except Exception:
+            return None
