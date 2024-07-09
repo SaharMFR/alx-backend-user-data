ecbc18db63562891faec6231f345420044de89f7
SaharMFR <sweetysahora@gmail.com>
2024-07-09 23:41:39 +0000

    Fixing pycodestyle in task-10 0x01

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -54,14 +54,14 @@ class BasicAuth(Auth):
                 not isinstance(user_email, str) or
                 not isinstance(user_pwd, str)):
             return None
-        
+
         try:
             users = User.search({'email': user_email})
         except Exception:
             return None
-        
+
         for user in users:
             if user.is_valid_password(user_pwd):
                 return user
-            
+
         return None
