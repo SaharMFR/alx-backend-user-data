17b516277a9bef18834dbef9306c3fcfc1029f0f
SaharMFR <sweetysahora@gmail.com>
2024-07-12 01:33:06 +0000

    Update1 in task-7 0x02

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -8,36 +8,26 @@ from os import getenv
 
 @app_views.route('/auth_session/login', methods['POST'], strict_slashes=False)
 def login():
-    """ POST /api/v1/auth_session/login
-    Return:
-      - User instance based on the email
-    """
     email = request.form.get('email')
-    if not email:
+    password = request.form.get('password')
+
+    if email is None or email == '':
         return jsonify({"error": "email missing"}), 400
 
-    password = request.form.get('password')
-    if not password:
+    if password is None or password == '':
         return jsonify({"error": "password missing"}), 400
 
-    try:
-        users = User.search({'email': email})
-    except Exception:
-        return jsonify({"error": "no user found for this email"}), 404
-
-    if not users:
+    users = User.search({"email": email})
+    if not users or users == []:
         return jsonify({"error": "no user found for this email"}), 404
 
     for user in users:
-        if not user.is_valid_password(password):
-            return jsonify({"error": "wrong password"}), 401
+        if user.is_valid_password(password):
+            from api.v1.app import auth
 
-    from api.v1.app import auth
-
-    user = users[0]
-    session_id = auth.create_session(user.id)
-    session_name = getenv('SESSION_NAME')
-    response = jsonify(user.to_json())
-    response.set_cookie(session_name, session_id)
-
-    return response
+            session_id = auth.create_session(user.id)
+            response = jsonify(user.to_json())
+            session_name = getenv('SESSION_NAME')
+            response.set_cookie(session_name, session_id)
+            return response
+    return jsonify({"error": "wrong password"}), 401
