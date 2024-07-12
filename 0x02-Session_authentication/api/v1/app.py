54e93e4168d875f976ac09404f40f56fe61a873c
SaharMFR <sweetysahora@gmail.com>
2024-07-12 00:12:50 +0000

    Task-5 0x02

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,10 +54,15 @@ def before_request() -> str:
 
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
 
     if not auth.require_auth(request.path, excluded_paths):
         return
+
+    if (not authauthorization_header(request) and not
+            auth.session_cookie(request)):
+        abort(401)
 
     if auth.authorization_header(request) is None:
         abort(401)
