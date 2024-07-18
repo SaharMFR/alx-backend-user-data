50658bcb623219dc1d57a8c4cd60125abc67fc38
SaharMFR <sweetysahora@gmail.com>
2024-07-18 12:17:56 +0000

    Task-8 0x03

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -26,3 +26,11 @@ class Auth:
             new_user = self._db.add_user(email, _hash_password(password))
             return new_user
         raise ValueError("User {} already exists".format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Validates user login """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
