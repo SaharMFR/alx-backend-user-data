13fb91a867f5d93fd197f001bd5d49736a485966
SaharMFR <sweetysahora@gmail.com>
2024-07-18 12:00:28 +0000

    Update in task-7 0x03

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -26,7 +26,7 @@ def users() -> str:
     password = request.form.get("password")
     try:
         AUTH.register_user(email, password)
-        return jsonify({"email": f"{email}", "message": "user created"})
+        return jsonify({"email": f"{email}", "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
