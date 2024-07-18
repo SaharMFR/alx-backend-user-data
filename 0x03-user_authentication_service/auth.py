b14d954b96e374ef5af819b3d79ed5502c125cb4
SaharMFR <sweetysahora@gmail.com>
2024-07-18 13:30:13 +0000

    Update1 in task-13 0x03

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -64,6 +64,6 @@ class Auth:
         """ Destroys a session by user ID """
         try:
             user = self._db.find_user_by(id=user_id)
-            self._db.update_user(id=user.id, session_id=None)
+            self._db.update_user(user.id, session_id=None)
         except NoResultFound:
             return
